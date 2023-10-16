daee700a9d1b2791da9a2db4f1d7f3a240864d8c
ArthurValada <arthur.c.valadares@gmail.com>
2023-10-16 13:21:12 -0300

    Adição dos módulos necessários

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/arthur/CLionProjects/praticalProject
+# Install script for directory: /home/arthur/CLionProjects/practicalProject
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/arthur/CLionProjects/praticalProject/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/arthur/CLionProjects/practicalProject/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
